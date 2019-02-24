072b9017d50a2c666dad94749d6290cb6d81c663
Matze1985 <mathias_noack@t-online.de>
2019-02-24 17:42:18 +0100

    Version 0.0.7 * Better check for missed values * Added color for strings * Remove notification time * Added better calculation for delta

--- script.glucotv/addon.py
+++ script.glucotv/addon.py
@@ -11,25 +11,25 @@ addonid = __addon__.getAddonInfo('id')
 addonicon = xbmc.translatePath(__addon__.getAddonInfo('icon'))
 
 # Info dialog for starting progran
-xbmcgui.Dialog().notification(addonname, 'Program started ...', xbmcgui.NOTIFICATION_INFO)
+xbmcgui.Dialog().notification(addonname, str('Add-on started ...'), xbmcgui.NOTIFICATION_INFO)
 
 while 1:
 	# Settings
 	sNightscout = __addon__.getSetting('nightscout')
-	sAlarm = __addon__.getSetting('alarm')
+	sSound = __addon__.getSetting('sound')
 	sNotification = __addon__.getSetting('notification')
 
 	# Load JSON from url
 	try:
-		urlEntries = urllib2.urlopen(sNightscout + '/api/v1/entries/sgv.json?count=2')
+		urlEntries = urllib2.urlopen(sNightscout + str('/api/v1/entries/sgv.json?count=3'))
 		jsonEntries = json.load(urlEntries)	
-		urlStatus = urllib2.urlopen(sNightscout + '/api/v1/status.json')
+		urlStatus = urllib2.urlopen(sNightscout + str('/api/v1/status.json'))
 		jsonStatus = json.load(urlStatus)
 	except urllib2.HTTPError, e:
-		xbmcgui.Dialog().notification(addonname, 'HTTPError: ' + str(e.code), xbmcgui.NOTIFICATION_ERROR)
+		xbmcgui.Dialog().notification(addonname, str('HTTP-Error: ') + str(e.code), xbmcgui.NOTIFICATION_ERROR)
 		sys.exit()
 	except urllib2.URLError, e:
-		xbmcgui.Dialog().notification(addonname, 'URLError: ' + str(e.args), xbmcgui.NOTIFICATION_ERROR)
+		xbmcgui.Dialog().notification(addonname, str('URL-Error: ') + str(e.args), xbmcgui.NOTIFICATION_ERROR)
 		sys.exit()
 
 	# Read glucose values
@@ -40,7 +40,12 @@ while 1:
 	iDate = int(jsonEntries[0]['date'])
 	iLastDate = int(jsonEntries[1]['date'])
 	iServerTimeEpoch = int(jsonStatus['serverTimeEpoch'])
+	
+	# Calculate and strings
 	iMsServerTimeEpochDate = iServerTimeEpoch - iDate
+	iMsInterval = iDate - iLastDate
+	iMinInterval = iMsInterval / 60000 + 1
+	iMinSecondInterval = iMinInterval * 2
 	iMin = iMsServerTimeEpochDate / 60000
 
 	# Calculate ms for sleep 
@@ -61,13 +66,13 @@ while 1:
 	iBgLow = int(jsonStatus['settings']['thresholds']['bgLow'])
 
 	# Check status of Nightscout
-	if sStatus <> 'ok':
-		xbmcgui.Dialog().notification(addonname, 'Please check the settings and the availability of URL! • ' + 'Status: [' + sStatus + ']', xbmcgui.NOTIFICATION_ERROR)
+	if sStatus <> str('ok'):
+		xbmcgui.Dialog().notification(addonname, str('Please check the settings and the availability of URL! • ') + str('Status: [' + sStatus + ']'), xbmcgui.NOTIFICATION_ERROR)
 
 	# Check if sUnits: mmol
 	i_fSgv = ''
 	i_fLastSgv = ''
-	if sUnits == 'mmol':
+	if sUnits == str('mmol'):
 		i_fSgv = round(float(iSgv * 0.0555), 1)
 		i_fLastSgv = round(float(iLastSgv * 0.0555), 1)
 	else:
@@ -75,7 +80,14 @@ while 1:
 		i_fLastSgv = iLastSgv
 
 	# Calculate delta
+	sTmpDelta = ''
 	i_fDelta = float(i_fSgv - i_fLastSgv)
+	sDirection = str(jsonEntries[0]['direction'])
+	if i_fSgv < i_fLastSgv:
+		if sDirection == str('FortyFiveUp') or sDirection == str('SingleUp') or sDirection == str('DoubleUp'):
+			sTmpDelta = str(i_fDelta.replace('-', ''))
+			i_fDelta = float(sTmpDelta)
+	
 	sDelta = ''
 	if i_fDelta > 0:
 		sDelta = str('+')
@@ -83,46 +95,34 @@ while 1:
 		sDelta = str('±')
 
 	# Notification
-	sHeader = str(i_fSgv) + ' ' + sUnits + ' • ' + sDelta + str(i_fDelta)
-	sMessage1 = str('just now')
-	sMessage2 = str(iMin) + str(' min ago')
-
-	if sNotification == '1 sec':
-		iNotificationTime = 1000
-	if sNotification == '2 sec':
-		iNotificationTime = 2000
-	if sNotification == '3 sec':
-		iNotificationTime = 3000
-	if sNotification == '4 sec':
-		iNotificationTime = 4000
-	if sNotification == '5 sec':
-		iNotificationTime = 5000
-	if sNotification == '6 sec':
-		iNotificationTime = 6000
-	if sNotification == '7 sec':
-		iNotificationTime = 7000
-	if sNotification == '8 sec':
-		iNotificationTime = 8000
-	if sNotification == '9 sec':
-		iNotificationTime = 9000
-	if sNotification == '10 sec':
-		iNotificationTime = 10000
-
-	# Alarm
-	bAlarm = ''
-	if iSgv <= iBgLow or iSgv >= iBgHigh or iSgv <= iBgTargetBottom or iSgv >= iBgTargetTop or iMin >= 10:
-		bAlarm = True
-	else:
-		bAlarm = False
+	sGlucose = str(i_fSgv) + str(' ') + sUnits + str(' • ') + sDelta + str(i_fDelta)
+	sJustNow = str('just now')
+	sMinAgo = str(iMin) + str(' min ago')
 	
+	# Color of strings
+	sColorYellow = str('[COLOR yellow]')
+	sColorRed = str('[COLOR red]')
+	sColor = str('[/COLOR]')
+
+	# Alerts - Red color
+	if iSgv <= iBgLow or iSgv >= iBgHigh or iSgv <= iBgTargetBottom or iSgv >= iBgTargetTop:
+		sGlucose = sColorRed + sGlucose + sColor
+	
+	# Sound - Notification
+	bSound = False
+	if sSound == str('On'):
+		bSound = True
+	
+	# Check of glucose
+	sMin = str(iMin)
+
 	if iMin == 0:
-		xbmcgui.Dialog().notification(sHeader, sMessage1, addonicon, iNotificationTime, bAlarm)
-	if iMin >= 10:
-		xbmcgui.Dialog().notification(sHeader, sMessage2, addonicon, iNotificationTime, bAlarm)
+		xbmcgui.Dialog().notification(sGlucose, sJustNow, addonicon, 5000, bSound)
+	if sMin.endswith(str(iMinInterval)[-1:]) or sMin.endswith(str(iMinSecondInterval)[-1:]):
+		sMinAgo = sColorYellow + sMinAgo + sColor
+		xbmcgui.Dialog().notification(sGlucose, sMinAgo, addonicon, 5000, bSound)
+	xbmc.sleep(iMsWait)
 	
 	# Close url
 	urlEntries.close()
 	urlStatus.close()
-
-	# Sleep
-	xbmc.sleep(iMsWait)
